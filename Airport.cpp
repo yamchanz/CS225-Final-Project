9206c050e905854a4cb8d1a73d370934e71727b2
Charlie Gordon <cgordon5@linux-a2.ews.illinois.edu>
2020-12-02 16:50:36 -0600

    Charlie test

--- Airport.cpp
+++ Airport.cpp
@@ -60,6 +60,25 @@ vector<Edge> Airport::findShortestPath(Graph g_, Vertex source, Vertex destinati
     for (int i = 0; i < vertices.size() - 1; i++) {
         int u = minDistance
     }
+    /*
+Dijkstra(Graph, source, destination):
+
+  initialize distances  // initialize tentative distance value
+  initialize previous   // initialize a map that maps current node -> its previous node
+  initialize priority_queue   // initialize the priority queue
+  initialize visited
+
+  while the top of priority_queue is not destination:
+      get the current_node from priority_queue
+      for neighbor in current_node's neighbors and not in visited:
+          if update its neighbor's distances:
+              previous[neighbor] = current_node
+      save current_node into visited
+
+  extract path from previous
+  return path and distance
+*/
+}
 
 // lat is first, long is second
 long double Airport::calcDistance(pair<double, double> coord1, pair<double, double> coord2){
@@ -80,27 +99,6 @@ pair<long double, long double> Airport::toRadians(const pair<double, double> coo
 	temp.first = coord.first * M_PI / 180;
 	temp.second = coord.second * M_PI / 180;
 	return temp; 
-}
-
-
-/*
-Dijkstra(Graph, source, destination):
-
-  initialize distances  // initialize tentative distance value
-  initialize previous   // initialize a map that maps current node -> its previous node
-  initialize priority_queue   // initialize the priority queue
-  initialize visited
-
-  while the top of priority_queue is not destination:
-      get the current_node from priority_queue
-      for neighbor in current_node's neighbors and not in visited:
-          if update its neighbor's distances:
-              previous[neighbor] = current_node
-      save current_node into visited
-
-  extract path from previous
-  return path and distance
-*/
 }
 
 
