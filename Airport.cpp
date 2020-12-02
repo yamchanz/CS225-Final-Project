9fc7b74cb55f97e9209d9ab941c26323a1fe2369
Your Name <netid@illinois.edu>
2020-12-02 13:53:12 -0600

    nickl changes

--- Airport.cpp
+++ Airport.cpp
@@ -28,6 +28,32 @@ double Airport::findDistance(pair<double, double> coord1, pair<double, double> c
     return temp;
 }
 
+vector<Edge> Airport::findShortestPath(Vertex code1, Vertex code2){
+/*
+Dijkstra(Graph, source, destination):
+
+  initialize distances  // initialize tentative distance value
+  initialize previous   // initialize a map that maps current node -> its previous node
+  initialize priority_queue   // initialize the priority queue
+  initialize visited
+
+  while the top of priority_queue is not destination:
+      get the current_node from priority_queue
+      for neighbor in current_node's neighbors and not in visited:
+          if update its neighbor's distances:
+              previous[neighbor] = current_node
+      save current_node into visited
+
+  extract path from previous
+  return path and distance
+*/
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
 /*
 Airport::Airport(string name, string city, string country, string code, double latitude, double longitude)
  : name_(name), city_(city), country_(country), code_(code), latitude_(latitude), longitude_(longitude)
--- Airport.h
+++ Airport.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 #include "graph.h"
 #include "edge.h"
