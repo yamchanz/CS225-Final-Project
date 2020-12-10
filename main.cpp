a63f6b76f2b8b13db03b576a1a0904dc98a85ecb
Nick Arand <ndarand2@linux-a2.ews.illinois.edu>
2020-12-10 16:56:33 -0600

    MAIN is up and working

--- main.cpp
+++ main.cpp
@@ -3,29 +3,91 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include "Values.h"
 
+using std::string;
 
-int main() {
+int main(int argc, char* argv[]) {
+    //Gives list of 100 airports based on argv2 number parameter 
+    if (argc == 3) {
+        string list = argv[1];
+        if (list == "list") {
+            string num = argv[2];
+            int index;
+            try {index = std::stoi(num);}
+            catch (std::exception& e) {
+                std::cout << "Not a valid number" << std::endl;
+                return 0;
+            }
+            //Populates Graph
+            if (index < 0 || index > 7400) {return 0;}
+            Airport airport("data/airports.txt","data/routes.txt");
+            std::cout << ":::Airports and Routes Populated:::" << std::endl;
+            int cnt = 0;
+            std::unordered_map<string, Values> airportList = airport.getAirportList();
+            for (auto i : airportList) {
+                cnt++;
+                if (cnt >= index) {
+                    std::cout << i.second.code_ << " , ";
+                    if (cnt >= index + 100) {break;}
+                }
+            }
+            std::cout << std::endl;
+
+            return 0;
+        }
+    }
+
+    //Makes sure the number of parameters is correct
+    if (argc < 4) {
+        std::cout << "\nFIND THE SHORTEST ROUTE, VIA DISTANCE OR # OF STOPS, USING : \n" << argv[0]
+        << " [Type of operation (DISTANCE or STOPS)] [3-letter airport code source] [3-letter airport code destination]\n\n"
+        << "TO GET A LIST OF 100 AIRPORT CODES IN OUR DATA, USE :\n"
+        << argv[0] << " list [number < 7400]\n\n"
+        << std::endl;
+        return 0;
+    }
+    //saves command-line arguments
+    string operation = argv[1];
+    string source = argv[2];
+    string destination = argv[3];
+
+    //standardizes inputs to all CAPS
+    string op = "";
+    for (char &c : operation) {
+        op += toupper(c);
+    }
+    string src = "";
+    for (char &c : source) {
+        src += toupper(c);
+    }
+    string des = "";
+    for (char &c : destination) {
+        des += toupper(c);
+    }
     
-    Airport airport("data/airports.txt","data/smallerRoutes.txt");
-    std::cout << "constructor done" << std::endl;
-    airport.drawMap();
-    //PNG* map = new PNG();
-    //map->readFromFile("mercatorMap.png");
-    //vector<Vertex> path = airport.findShortestWeightedPath("C:6","C13");
-    //airport.drawPath(path);
-    /*airport.drawMapHelper(map, -88,0, 88, 0);
-    airport.drawMapHelper(map, -88,-179, 88, 179);
-    airport.drawMapHelper(map, 88,-179, -88, 179);
-    airport.drawMapHelper(map, -44,-179, 44, 179);
-    airport.drawMapHelper(map, 44,-179, -44, 179);
-    airport.drawMapHelper(map, -22,-179, 22, 179);
-    airport.drawMapHelper(map, 22,-179, -22, 179);
-    airport.drawMapHelper(map, -11,-179, 11, 179);
-    airport.drawMapHelper(map, 11,-179, -11, 179);
-    airport.drawMapHelper(map, 0,-179, 0, 179);
-    // ORD to LHR
-    airport.drawMapHelper(map, 41.9742, -87.9073, 51.4700, -0.4543);*/
-    //map->writeToFile("Out.png");
+    //Populates Graph
+    Airport airport("data/airports.txt","data/routes.txt");
+    std::cout << ":::Airports and Routes Populated:::" << std::endl;
+
+    if (op == "DISTANCE") {
+        std::cout << "calculating best route..." << std::endl;
+        vector<Vertex> path = airport.findShortestWeightedPath(src,des);
+        airport.drawPath(path);
+        if (path.size() != 0) {
+            std::cout << ":::Shortest Route Drawn on Out.png:::" << std::endl;
+        }
+    } else if (op == "STOPS") {
+        std::cout << "calculating best route..." << std::endl;
+        vector<Vertex> path = airport.findShortestUnweightedPath(src,des);
+        airport.drawPath(path);
+        if (path.size() != 0) {
+            std::cout << ":::Shortest Route Drawn on Out.png:::" << std::endl;
+        }
+    } else {
+        std::cout << "Not a valid operation parameter.  Chooose between DISTANCE or STOPS to find shortest route of the respective type."
+        << std::endl;
+    }
     return 0;
 }
